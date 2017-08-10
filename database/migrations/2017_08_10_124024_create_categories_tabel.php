2d1bda5829659f117cf5e66495ceb35234b92c5f
Arne Van Bavel <arne.vanbavel@student.kdg.be>
2017-08-10 14:53:08 +0200

    Toevoegen db categories

---
+++ database/migrations/2017_08_10_124024_create_categories_tabel.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTabel extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('url');
+            $table->string('name');
+            $table->string('meervoudnaam');
+            $table->string('kleur');
+            $table->string('wit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
