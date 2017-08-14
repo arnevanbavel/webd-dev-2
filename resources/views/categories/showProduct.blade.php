8fc16e04afc4517fe6ea0b79bd38f55f5b7beded
Arne Van Bavel <arne.vanbavel@student.kdg.be>
2017-08-14 16:24:49 +0200

    subsribe product page

--- resources/views/categories/showProduct.blade.php
+++ resources/views/categories/showProduct.blade.php
@@ -136,29 +136,7 @@
 				</div>
 			</div>
 
-			<div class="subscribe-wrapper">
-
-                <div class="subscribe-info col-md-8">
-                    <h1><strong>{{ Lang::get('subscribe.deals') }}</strong></h1>
-                    <h4>{{ Lang::get('subscribe.only') }}</h4>
-                </div>
-
-                <div class="subscribe-form-wrapper col-md-4">
-                    <div class="susbcribe-form">
-                        <h4>{{ Lang::get('subscribe.subscribe') }}</h4>
-                        Lorum ipsum dolor sit amet...
-                        <form method="POST" action="{{ url('/subscribe') }}" class="subscribe">
-                            {{ csrf_field() }}
-                            <input type="text" name="email" value="{{old('email')}}"><button class="btn-primary btn-subscribe">OK</button>
-                            <br>
-                            @if ($errors->has('email'))
-                                <span class="form-validation"><strong>{{ $errors->first('email') }}</strong></span>
-                            @endif
-                        </form>
-                    </div>
-                </div>
-
-            </div>
+			@include('inc.subscribe')
 
 		</div>
 
