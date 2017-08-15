44375e9ce96a61ce6175d7bb0fadf87d7e5879a2
Arne Van Bavel <arne.vanbavel@student.kdg.be>
2017-08-15 21:20:18 +0200

    pageloader

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,10 @@
     <link href="{{ asset('slick-1.6.0/slick/slick-theme.css') }}" rel="stylesheet">
     <link href='http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css' rel='stylesheet' type='text/css'>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('pageloader/pageloader.css') }}" rel="stylesheet">
 </head>
 <body>
+    @include('inc.pageloader')
     <div class="container">
             <!--coockie-->
             <!--coockie-->
@@ -32,5 +34,6 @@
     <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script type="text/javascript" src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script src="{{ asset('slick-1.6.0/slick/slick.min.js') }}"></script>
+    <script src="{{ asset('pageloader/pageloader.js') }}"></script>
 </body>
 </html>
